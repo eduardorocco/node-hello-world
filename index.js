6d363f7a0a49ad382940c6ed565ca2a889841651
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-14 17:14:34 +0100

    ADD word by terminal

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Hello Boolean');
+
+// console.log(process);
+
+console.log(process.argv);
+
+const params = process.argv.slice(2)
+
+console.log(params);
+
+console.log(`Hello ${params}`);
